11e4d7bdaa159332da251ee1f773f91c100e40e9
Nereida Rodríguez <nereida@LAPTOP-VAJBTHLP.localdomain>
2022-07-06 09:09:38 +0200

    added math function and event

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+//Variables
+
+const inputNumber = document.querySelector('.js-your-number');
+const button = document.querySelector('.js-button');
+const clueParagraph = document.querySelector('.js-clue');
+const triesParagraph = document.querySelector('.js-tries');
+const yourNumber = inputNumber.value; //esto va dentro de función
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+};
+
+
+button.addEventListener('click', handleClick);
